81b116b6b3a7706fe601ec3b76514b444f94cd11
Arjun Vinod M <marjunvinod@gmail.com>
2023-09-07 22:15:33 +0530

    commit 2

--- vyaparapp/views.py
+++ vyaparapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-  return render(request, 'base.html')
+  return render(request, 'base1.html')
 
 def register(request):
   return render(request, 'register.html')
